fa45a214d3d9b1752b9beb6462c598eb478eea99
thereyouhaveit1 <40688410+thereyouhaveit1@users.noreply.github.com>
2019-05-10 16:34:22 -0700

    Add files via upload

---
+++ PrivateChatRoom.kt
@@ -0,0 +1,12 @@
+package com.example.workbug
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PrivateChatRoom : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_private_chat_room)
+    }
+}
---
+++ ProfilePage.kt
@@ -0,0 +1,13 @@
+package com.example.workbug.ui.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workbug.R
+
+class ProfilePage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_page)
+    }
+}
